294fc37ad73df697f36afa6d6314b42eb246cd1e
shivam-kumar-codes <sk9313589721@gmail.com>
2024-07-17 16:56:39 +0530

    first commit

---
+++ src/data/ProjectData.js
@@ -0,0 +1,74 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_1.png",
+      title: "Spotify",
+      description: " A music streaming application, replicating the popular features and functionalities of Spotify.",
+      tech_stack: "React JS, HTML, CSS, Javascript",
+      github_url: "https://github.com/shivam-kumar-codes",
+      demo_url: "https://next13-spotify.vercel.app/",
+    },
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_2.png",
+      title: "Realtime Chat App",
+      description:
+        "A react based web application which allow users to chat in real time.",
+      tech_stack: "Html, Css, Javascript",
+      github_url: "https://github.com/shivam-kumar-codes/wassup",
+      demo_url: "https://wassup-three.vercel.app/",
+    },
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/portfolio.png",
+      title: "Portfolio",
+      description: "Ofcourse i made my own personal portfolio website.",
+      tech_stack: "Reactjs, Css",
+      github_url: "https://your_project_github_link",
+      demo_url: "https://your_project_demo_link/",
+    },
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_3.png",
+      title: "Textutils Web App",
+      description:
+        "A text formatting web app, providing a user-friendly interface for various text manipulation tasks.",
+      tech_stack: "Reactjs, Css",
+      github_url: "https://github.com/shivam-kumar-codes/Textutils",
+      demo_url: "https://shivam-kumar-codes.github.io/Textutils/",
+    },
+  ];
+  
+  // --------------------------------------- Skills
+  
+  export const stackList = [
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
+      name: "HTML",
+    },
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/css3.svg",
+      name: "CSS",
+    },
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/javascript.svg",
+      name: "JavaScript",
+    },
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/react.svg",
+      name: "ReactJS",
+    },
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/bootstrap.svg",
+      name: "Bootstrap",
+    },
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/sass.svg",
+      name: "Scss",
+    },
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
+      name: "Git",
+    },
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
+      name: "Responsive",
+    },
+  ];
